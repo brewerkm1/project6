1ae8a2ac1d0c79dc0d254b741d3d50626200db40
Kurt Brewer <BREWERKM1@ETSU.EDU>
2022-11-19 13:59:42 -0500

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project6
+{
+    internal class Book
+    {
+        string Title { get; set; } 
+        string Author { get; set; }
+        int Pages { get; set; }
+        string Publisher { get; set; }
+
+        public void Print(string Title, string Author, int Pages, string Publisher)
+        {
+            Console.WriteLine("Title: " + Title + "\nAuthor: " + Author + "\nPages: " + Pages + "\nPublisher: " + Publisher);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace project6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\xcutf\source\repos\project6\books.csv");
+
+        }
+    }
+}
